bb40a5a02e9a9eb38feddbfd8259efbd560c251c
Vishnu <visank@microsoft.com>
2021-10-12 23:07:36 -0700

    testing ci pipe once more

--- src/App.js
+++ src/App.js
@@ -4,12 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Meet your mentor
         </p>
-      </header>
     </div>
   );
 }
