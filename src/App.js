e486cfaa7eef0a4a86f4d42e0faf75804f17aba3
Vishnu <visank@microsoft.com>
2021-10-12 19:03:52 -0700

    checking build line

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Meet your mentor
         </a>
       </header>
     </div>
